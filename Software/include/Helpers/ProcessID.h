03624efa614d5bc91d5158c89b6737cdc586cf55
Seth Zegelstein <seze4251@colorado.edu>
2017-01-15 13:23:34 -0700

    All Processes Talk to WatchDog

---
+++ Software/include/Helpers/ProcessID.h
@@ -0,0 +1,22 @@
+//
+//  ProcessID.h
+//  ProcessID
+//
+//  Created by Seth on 11/24/2016.
+//  Copyright © 2016 Seth. All rights reserved.
+//
+
+#ifndef PROCESSID_H
+#define PROCESSID_H
+
+enum ProcessID {
+    P_NA = -1,
+    P_CameraController = 1,
+    P_ScComms = 2,
+    P_GNC = 3,
+    P_ImageProcessor = 4,
+    P_WatchDog = 5,
+    P_Spacecraft = 6,
+};
+
+#endif
