d09ace3895785d16d938112efc71e3370f3d9bbc
Heptoxide <anto8214@colorado.edu>
2017-04-02 20:34:41 -0600

    testAnalyzeImage debugging

--- test/testAnalyzeImage.cpp
+++ test/testAnalyzeImage.cpp
@@ -34,12 +34,16 @@ void calcRadGuess(double* pxDiam, double* estPos, PointEarthMoon point, double* 
 MAIN SCRIPT
 */
 int main(int argc, char **argv) {
+	std::cout << "Starting main" << std::endl;
 	//Setup time
 	time_t rawtime;
 	struct tm * timinfo;
 
 	// Set up analysis variables
 	double pix_deg[2] = {72, 72};
+	double cameraWidth = 4120.0;
+	double cameraHeight = 3260.0;
+
 	double estPos[3] = {21212.105392, 25545.913981, 7945.349932};
 	double moonEphem[3] = {-20878.747372, 347641.764991, 132624.248850};
 	PointEarthMoon emPt = PEM_Earth;
@@ -49,9 +53,14 @@ int main(int argc, char **argv) {
 
 	//setImageParameters(emPt, pix_deg, estPos, moonEphem, sensitivity, radGuessIn);
 
-	unsigned char imIn[2428800];
+	std::cout << "Allocating image memory" << std::endl;
+	//unsigned char imIn[2428800];
+	unsigned char* imIn = new unsigned char[40293600];
 
-	readImage("test/TestImages/moonTest.jpeg", imIn);
+	readImage("test/TestImages/moonTest_old.jpg", imIn);
+
+	//cv::imshow("Image", &imIn);
+	//cv::waitKey(0);
 
 	//Output variables
 	double centerPt_data[2];
@@ -59,8 +68,6 @@ int main(int argc, char **argv) {
 	double radius;
 	double numCirc;
 	double alpha, beta, theta;
-	double cameraWidth = 4120.0;
-	double cameraHeight = 3260.0;
 
 	try{
 		analyzeImage(imIn, radGuessIn, sensitivity, centerPt_data, centerPt_size, 
@@ -259,13 +266,17 @@ void readImage(std::string imgFilename, unsigned char* imIn) {
             uchar blue = intensity.val[0];
             uchar green = intensity.val[1];
             uchar red = intensity.val[2];
-            imIn[counter] = red;
-            imIn[counter + 809600] = green;
-            imIn[counter + 2 * 809600] = blue;
+            //imIn[counter] = red;
+            //imIn[counter + 13431200] = green;
+            //imIn[counter + 2 * 13431200] = blue;
+            *(imIn + counter) = red;
+            *(imIn + counter + 13431200) = green;
+            *(imIn + counter + 2*13431200) = blue;
             counter++;
         }
     }
 
+    //cv::imshow("Display window", imIn);
 
 	//imageMessage->currentImageSize = 2428800;
 	printf("Read Image: Finished Image Read\n");
